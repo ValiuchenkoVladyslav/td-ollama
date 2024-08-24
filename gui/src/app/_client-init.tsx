92f3f3e9c4384a926f1e9ad61c58003c218e2e52
wladyslaw <vladislavvaliuchenkov@gmail.com>
2024-08-24 19:23:51 +0100

    extended initalization logic

--- gui/src/app/_client-init.tsx
+++ gui/src/app/_client-init.tsx
@@ -1,23 +1,33 @@
 "use client";
 
+import { useAppStore } from "~/store";
+
 export default function ClientInit() {
-  if (typeof window !== "undefined" && process.env.NODE_ENV !== "development") {
-    // disable some browser shortcuts
-    window.addEventListener("keydown", (e) => {
-      if (
-        ["r", "f"].includes(e.key.toLowerCase()) &&
-        (e.ctrlKey || e.metaKey)
-      ) e.preventDefault();
+  if (typeof window !== "undefined") {
+    // init store on client side
+    useAppStore.getState().initStore();
 
-      if (e.key === "F5") e.preventDefault();
-    });
+    // disable some browser features so app feels more native
+    if (process.env.NODE_ENV !== "development") {
+      window.addEventListener("keydown", (e) => {
+        if (
+          ["r", "f"].includes(e.key.toLowerCase()) &&
+          (e.ctrlKey || e.metaKey)
+        ) e.preventDefault();
 
-    // disable right click context menu
-    function preventContextMenu(e: MouseEvent) {
-      e.preventDefault();
+        if (e.key === "F5") e.preventDefault();
+      });
+
+      window.addEventListener("contextmenu", (e) => e.preventDefault());
+
+      const noSelectStyles = document.createElement("style")
+      noSelectStyles.textContent = `
+        * {
+          user-select: none;
+        }
+      `;
+      document.head.appendChild(noSelectStyles);
     }
-
-    window.addEventListener("contextmenu", preventContextMenu); 
   }
 
   return null;
